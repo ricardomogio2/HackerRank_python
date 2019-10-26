14d63f0b2ae47cdfff9608f5e37c966e6ea7e88f
RicardoMogio <47612007+ricardomogio2@users.noreply.github.com>
2019-10-26 11:23:29 -0300

    Update and rename hello_world.py to 00_helloWorld.py

---
+++ 00_helloWorld.py
@@ -0,0 +1,3 @@
+#Python 3 code - HackerRank solution for "Say Hello World in Python"
+
+print("Hello, World!")
--- hello_world.py
+++
@@ -1,3 +0,0 @@
-'''Python 3 code - HackerRank solution for "Say Hello World in Python'''
-
-print("Hello, World!")
