3377eead6927aa67ce47217a9e7f6c534d20f76c
RicardoMogio <47612007+ricardomogio2@users.noreply.github.com>
2019-10-26 13:10:45 -0300

    Create 05_WriteAFunction.py

---
+++ 05_WriteAFunction.py
@@ -0,0 +1,13 @@
+def is_leap(year):  #Writing the function
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+year = int(input())
+print(is_leap(year)) #Calling the function plus a print
